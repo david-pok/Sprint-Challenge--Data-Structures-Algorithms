446b4793199468fe1da20ca53673a2d997739ba1
David Pok <david.pok@gmail.com>
2018-10-12 10:03:09 -0700

    Am

--- data_structures/ex_2/heap.py
+++ data_structures/ex_2/heap.py
@@ -1,20 +1,24 @@
-def heapify(arr, n, i):
-    # Find largest among root and children
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
-
-    if l < n and arr[i] < arr[l]:
-        largest = l
-
-    if r < n and arr[largest] < arr[r]:
-        largest = r
-
-    # If root is not largest, swap with largest and continue heapifying
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
-
+def heapify(arr, n, i): 
+    largest = i # Initialize largest as root 
+    l = 2 * i + 1     # left = 2*i + 1 
+    r = 2 * i + 2     # right = 2*i + 2 
+  
+    # See if left child of root exists and is 
+    # greater than root 
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+  
+    # See if right child of root exists and is 
+    # greater than root 
+    if r < n and arr[largest] < arr[r]: 
+        largest = r 
+  
+    # Change root, if needed 
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i] # swap 
+  
+        # Heapify the root. 
+        heapify(arr, n, largest) 
 
 def heapsort(arr):
     n = len(arr)
